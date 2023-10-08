46c4a6b2a32e573375fe912f06dee42a94cacac3
AlphaCentauri4 <94246993+AlphaCentauri4@users.noreply.github.com>
2023-10-07 22:35:53 -0400

    Delete fftstack-icmla/spatiotemporal/mask directory

--- fftstack-icmla/spatiotemporal/mask/load-area-mask.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-area = np.fromfile("spatiotemporal/mask/psn25area_v3.dat", dtype=np.dtype('<i'))
-area=area.reshape(448,304)
-area_size = area/1000.0
-np.save('spatiotemporal/mask/area_mask.npy', area_size)
